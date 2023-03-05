cf23bf6381c7bb94318dad0b58e82b7947edce47
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-05 22:43:03 +0400

    AdOps and Admins models created

--- src/tDSP/dsp/admin.py
+++ src/tDSP/dsp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models.admin_model import AdminModel
+from .models.ad_ops_model import AdOpsModel
+
+
+class AdminAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'photo_name')
+
+    @admin.display(description='Photo')
+    def photo_name(self, obj):
+        if obj.photo:
+            return format_html('<a href="{}">...{}</a>', obj.photo.url, obj.photo.name[-12:])
+
+
+class AdOpsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'photo_name')
+
+    @admin.display(description='Photo')
+    def photo_name(self, obj):
+        if obj.photo:
+            return format_html('<a href="{}">...{}</a>', obj.photo.url, obj.photo.name[-12:])
+
+
+admin.site.register(AdminModel, AdminAdmin)
+admin.site.register(AdOpsModel, AdOpsAdmin)
--- src/tDSP/dsp/models.py
+++ src/tDSP/dsp/models.py
@@ -1,3 +1,2 @@
-from django.db import models
-
-# Create your models here.
+from .models.admin_model import AdminModel # noqa
+from .models.ad_ops_model import AdOpsModel # noqa
