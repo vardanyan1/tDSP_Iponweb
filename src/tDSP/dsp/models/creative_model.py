4c8b1e007321dd304d64cc84702522f6451b1ef6
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-08 22:32:29 +0400

    creative_model.py created

---
+++ src/tDSP/dsp/models/creative_model.py
@@ -0,0 +1,17 @@
+from django.db import models
+from ..models.categories_model import SubcategoryModel
+from ..models.campaign_model import CampaignModel
+
+
+class CreativeModel(models.Model):
+
+    external_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    image_url = models.URLField(max_length=500)
+    categories = models.ManyToManyField(SubcategoryModel)
+    campaign = models.ForeignKey(CampaignModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.external_id})"
