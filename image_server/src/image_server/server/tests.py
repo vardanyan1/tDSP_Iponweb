a45d9ff4c3e2203a3a81c3786d101c78a8c3ef02
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-10 16:50:58 +0400

    image_server modified to support json instead of multipart and base64 images Tests modified too

--- image_server/src/image_server/server/tests.py
+++ image_server/src/image_server/server/tests.py
@@ -1,9 +1,11 @@
+import base64
 from PIL import Image as pil
 from io import BytesIO
 
+from django.core.files.base import ContentFile
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 from .models import Image, generate_image_filename
@@ -14,34 +16,48 @@ class ImageTestCase(TestCase):
         self.client = APIClient()
         self.image_data = {'file': SimpleUploadedFile('test_image.png', b'')}
 
+    @staticmethod
+    def create_test_image():
+        # Create a 100x100 pixel RGB image with a red background
+        img = pil.new('RGB', (100, 100), color='red')
+
+        # Encode the image as PNG and get the bytes
+        img_bytes = BytesIO()
+        img.save(img_bytes, format='PNG')
+        img_bytes = img_bytes.getvalue()
+
+        # Encode the image bytes as base64
+        encoded_image = base64.b64encode(img_bytes)
+
+        return encoded_image
+
     def test_generate_image_filename(self):
         instance = None
         filename = 'test_image.png'
         result = generate_image_filename(instance, filename)
         self.assertRegex(result, r'^[A-Za-z0-9_]{32}\.png$')
 
-    def test_image_save(self):
-        image = Image.objects.create(file=SimpleUploadedFile('test_image.png', b''))
+    def test_image_creation(self):
+        # Create a test image
+        image_data = self.create_test_image()  # base64 encoded image data
+        image_file = ContentFile(base64.b64decode(image_data), name='test.png')
+
+        # Create an Image object
+        image = Image.objects.create(file=image_file)
+
+        # Check that the file was saved correctly
+        self.assertEqual(image.file.read(), base64.b64decode(image_data))
         self.assertEqual(image.url, f'http://localhost:8001/media/{image.file.name}')
 
-    @staticmethod
-    def create_test_image():
-        # Create a 100x100 pixel RGB image with a red background
-        img = pil.new('RGB', (100, 100), color='red')
-        # Save the image to a byte stream
-        stream = BytesIO()
-        img.save(stream, format='PNG')
-        # Return the byte stream content as bytes
-        return stream.getvalue()
-
     def test_save_image_view(self):
-        file_data = {'file': SimpleUploadedFile('test_image.png', self.create_test_image())}
-        response = self.client.post('/save_image/', file_data, format='multipart')
-        if response.status_code != status.HTTP_201_CREATED:
-            print(response.data)
+
+        image_data = self.create_test_image()  # base64 encoded image data
+
+        response = self.client.post('/save_image/', {'file': image_data}, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertRegex(response.data['url'], r'^http://localhost:8001/media/[A-Za-z0-9_]{32}\.png$')
 
     def test_save_image_view_invalid_data(self):
-        response = self.client.post('/save_image/', {}, format='multipart')
+        response = self.client.post('/save_image/', {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- image_server/src/image_server/server/views.py
+++ image_server/src/image_server/server/views.py
@@ -1,13 +1,8 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from .models import Image
 from .serializers import ImageSerializer
 
 
-class SaveImageView(APIView):
-    def post(self, request, format=None):
-        serializer = ImageSerializer(data=request.data)
-        if serializer.is_valid():
-            image = serializer.save()
-            return Response({'url': image.url}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SaveImageView(generics.CreateAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
