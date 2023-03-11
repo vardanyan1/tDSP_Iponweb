d11a0844d821e59651b993380a9e1eb96bfd343e
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-11 21:49:44 +0400

    Setup CI

---
+++ django/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+#python manage.py flush --no-input
+python src/manage.py migrate
+
+if [ ! -f categories/initiated.txt ]; then
+    python src/manage.py import_categories categories/Content-Taxonomy-1.0.xlsx
+    touch categories/initiated.txt
+fi;
+
+exec "$@"
