73b71c771dbba0278e644a7dbce4d108642e8bcd
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-12 15:29:23 +0400

    settings DEBUG modify

--- django/src/tdsp/settings.py
+++ django/src/tdsp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ast.literal_eval(os.environ.get('DEBUG'))
+DEBUG = ast.literal_eval(str(os.environ.get('DEBUG')))
 
 ALLOWED_HOSTS = ['*']
 
