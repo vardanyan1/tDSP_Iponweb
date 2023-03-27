1ea01fe81e4b99f9b479693f16cbf5241826e2c3
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-28 01:53:28 +0400

    Automate Prometheus and Grafana setup on build for dev and prod

--- django/src/tdsp/settings.py
+++ django/src/tdsp/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'tdsp.wsgi.application'
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
+        'ENGINE': 'django_prometheus.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
