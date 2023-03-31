008efdc27616315de9990f79cedd752e4550fd31
vardanyan1 <vardanyaninbox@gmail.com>
2023-04-01 01:36:00 +0400

    Modify migrations and web part of docker-compose

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -8,14 +8,9 @@ done
 
 echo "PostgreSQL started"
 
-#  TODO:Write better code cuz if theres no migrations folder this wont work
-# Check if migrations have been applied for the dsp app
-#if python src/manage.py showmigrations | grep -q "(no migrations)"; then
-
-# Check if there is not migrations  folder apply migrations and fill the categories and subcategories
-if [ ! -d "src/tdsp/dsp/migrations" ]; then
-    echo "Migrations for dsp app not applied. Running makemigrations and migrate..."
-    python src/manage.py makemigrations dsp
+# Check if Categories model exists in the database
+if python src/manage.py check_categories_exists | grep -q "Categories model does not exist"; then
+    echo "Migrations for dsp app not applied. Running migrate..."
     python src/manage.py migrate
     python src/manage.py import_categories categories/Content-Taxonomy-1.0.xlsx
     echo "Collecting static files..."
@@ -25,7 +20,7 @@ if [ ! -d "src/tdsp/dsp/migrations" ]; then
     python src/manage.py createsuperuser --email=admin@admin.com --noinput
     echo "Superuser created!"
 else
-    echo "Migrations for dsp app already applied. Skipping makemigrations and migrate."
+    echo "Migrations for dsp app already applied. Skipping and migrate."
 fi
 
-exec "$@"
+exec /bin/sh -c "cd src/ && gunicorn --forwarded-allow-ips='*' --bind 0.0.0.0:8000 tdsp.wsgi"
