7a991a93ec6b3c1c1da843b2d792e0d6140f8379
vardanyan1 <vardanyaninbox@gmail.com>
2023-04-11 13:18:35 +0400

    Fix Migration for Campaign name length

--- django/src/tdsp/dsp/migrations/0001_initial.py
+++ django/src/tdsp/dsp/migrations/0001_initial.py
@@ -42,7 +42,7 @@ class Migration(migrations.Migration):
             name='CampaignModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=30)),
+                ('name', models.CharField(max_length=128)),
                 ('budget', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('is_active', models.BooleanField(default=True)),
                 ('min_bid', models.DecimalField(decimal_places=2, default=None, max_digits=10, null=True)),
